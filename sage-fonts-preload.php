12ee91f005e1d0c4642c4845c5108d48a88465c1
huubl <>
2022-04-04 16:47:02 +0200

    Update
    - remove acorn dep. Check sage theme support
    - allow Sage 10 Bud and Laravel Mix

--- sage-fonts-preload.php
+++ sage-fonts-preload.php
@@ -1,22 +1,27 @@
 <?php
-use Illuminate\Support\Str;
 
+use Illuminate\Support\Str;
 use function Roots\asset;
 
-if (! function_exists('add_filter')) {
-    return;
+if (! function_exists('get_theme_support') && get_theme_support( 'sage') !== true) {
+	return;
 }
 
 add_filter('wp_head', function () {
-    echo collect(
-        json_decode(asset('mix-manifest.json')->contents())
-    )->keys()->filter(function ($item) {
-        return Str::endsWith($item, ['.otf', '.eot', '.woff', '.woff2', '.ttf']);
-    })->map(function ($item) {
-        // Return asset uri without versioning query string
-        return sprintf(
-            '<link rel="preload" href="%s" as="font" crossorigin>',
-            substr(asset($item)->uri(), 0, strpos(asset($item)->uri(), '?id='))
-        );
-    })->implode("\n");
+	$is_bud = asset('manifest.json')->exists() ? true : false; // Check if Sage 10 is using Bud or Laravel Mix
+
+	echo collect(
+		json_decode(asset(($is_bud ? 'manifest.json' : 'mix-manifest.json'))->contents())
+	)->keys()->filter(function ($item) {
+		return Str::endsWith($item, ['.otf', '.eot', '.woff', '.woff2', '.ttf']);
+	})->map(function ($item) use($is_bud) {
+		$asset_uri = $is_bud ? asset($item)->uri() : substr(asset($item)->uri(), 0, strpos(asset($item)->uri(), '?id='));
+
+		// Return asset uri without versioning query string
+		return sprintf(
+			'<link rel="preload" href="%s" as="font" crossorigin>',
+			$asset_uri
+		);
+	})->implode("\n");
 });
+
